5cfa76dae4dfdc96a4b1370bacf22ea336192175
Joshua Moody <joshuajmoody@gmail.com>
2018-05-25 05:14:55 +0200

    daily/iOS: run doctor on Xcode 8.3.3 and 9.2 simulators

--- ios/daily/core-simulator.sh
+++ ios/daily/core-simulator.sh
@@ -2,3 +2,14 @@
 
 rm -rf /Users/jenkins/Library/Logs/CoreSimulator
 mkdir -p /Users/jenkins/Library/Logs/CoreSimulator
+
+gem install run_loop
+
+developer_dirs=(
+"/Xcode/8.3.3/Xcode.app/Contents/Developer"
+"/Xcode/9.2/Xcode.app/Contents/Developer"
+)
+
+for directory in ${developer_dirs[@]}; do
+  DEVELOPER_DIR="${directory}" run-loop simctl doctor --debug
+done
