49079fd648cfd0134abeccc7fd5eae3407269ac2
Joshua Moody <joshuajmoody@gmail.com>
2018-06-21 07:06:59 +0200

    ios/hourly: pull run-loop develop and attemp to install

--- ios/hourly/core-simulator.sh
+++ ios/hourly/core-simulator.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-gem install run_loop
+trap 'gem install run_loop' EXIT
+
+set -e
+
+gem uninstall -Vax --force --no-abort-on-dependent run_loop
+rm -rf run_loop
+git clone git@github.com:calabash/run_loop.git
+(cd run_loop; rake install)
+
 DEBUG=1 run-loop simctl manage-processes
